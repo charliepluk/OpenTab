6584f41cc555a14a12afbb7f587a1439d267fbeb
Charlie Pluk <charliepluk@users.noreply.github.com>
2020-06-21 13:20:10 -0400

    more navigation functionality

--- stylesheet/navbarStyle.js
+++ stylesheet/navbarStyle.js
@@ -3,11 +3,35 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
   homeNavBar: {
     height: 120,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
   },
 
   navBar: {
     height: 120,
     backgroundColor: "#FF9466",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+
+  navTab: {
+    height: 40,
+    width: 40,
+    backgroundColor: "blue",
+    marginTop: 10,
+    marginLeft: 20,
+  },
+
+  orderTab: {
+    height: 40,
+    width: 40,
+    backgroundColor: "blue",
+    marginTop: 10,
+    marginRight: 20,
   },
 });
 
